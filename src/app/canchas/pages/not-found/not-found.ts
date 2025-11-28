41e46741438df81ff3d00bf4c0e0fb83aec2451c
Ian <ian_huth_@hotmail.com>
2025-11-28 01:57:53 -0300

    added style to not found component

--- src/app/canchas/pages/not-found/not-found.ts
+++ src/app/canchas/pages/not-found/not-found.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css'
 })
 export class NotFound {
+  private readonly router = inject(Router);
 
+  protected goHome(): void {
+    this.router.navigate(['/explorar']);
+  }
 }
