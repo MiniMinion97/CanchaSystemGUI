93ae00077970ac7ab750fb9cf027ea8106d489c4
Ian <ian_huth_@hotmail.com>
2025-11-28 02:06:54 -0300

    added style to owner contact component

--- src/app/canchas/pages/owner-contact/owner-contact.ts
+++ src/app/canchas/pages/owner-contact/owner-contact.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './owner-contact.css'
 })
 export class OwnerContact {
-
+   protected openWhatsApp(): void {
+    const phoneNumber = '5492235348845';
+    const message = encodeURIComponent('¡Hola! Me gustaría obtener más información sobre el servicio para dueños de canchas.');
+    const url = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(url, '_blank');
+  }
 }
