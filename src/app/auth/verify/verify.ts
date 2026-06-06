2478a5c27c215a58d28b4e45e45e6da486a20fdb
6-LL <lorenzanilautaro+github@gmail.com>
2026-06-06 14:34:45 -0300

    verify component added

---
+++ src/app/auth/verify/verify.ts
@@ -0,0 +1,31 @@
+import { Component, inject, signal } from '@angular/core';
+import { VerificationService } from '../services/verification-service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-verify',
+  imports: [],
+  templateUrl: './verify.html',
+  styleUrl: './verify.css',
+})
+export class Verify {
+  private readonly verificationService = inject(VerificationService);
+  private readonly route = inject(ActivatedRoute);
+  private readonly token = this.route.snapshot.paramMap.get('token');
+  private readonly router = inject(Router);
+  protected message = signal<string>("Verificando cuenta...");
+
+  ngOnInit() {
+    this.verificationService.verifyToken(this.token!).subscribe({
+      next: () => {
+        this.message.set("Verificación completada. Regístrese para ingresar")
+        setTimeout(() => {
+          this.router.navigate(['/explorar']);
+        }, 5000);
+      },
+      error: () => {
+        this.message.set("La verificación ha expirado. Intente registrarse nuevamente.")
+      }
+    })
+  }
+}
