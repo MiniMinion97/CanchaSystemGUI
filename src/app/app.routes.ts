5f0bcf9e7cbe230ba488209cb3c924a9502b0431
6-LL <lorenzanilautaro+github@gmail.com>
2026-07-23 14:39:00 -0300

    Map added to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -29,8 +29,11 @@ import { AdminReservation } from './AdminComponents/admin-reservation/admin-rese
 import { AdminReviews } from './AdminComponents/admin-reviews/admin-reviews';
 import { NotFound } from './canchas/pages/not-found/not-found';
 import { Verify } from './auth/verify/verify';
+import { Map } from './canchas/map/map';
 
 export const routes: Routes = [
+    {path: 'mapa', component: Map},
+
     {path: 'explorar', component: Explore},
 
     {path: 'verificar/:token', component: Verify},
