2e8d92ed627b3018fd089e737267e4b6b91c0111
6-LL <lorenzanilautaro+github@gmail.com>
2026-07-24 18:41:31 -0300

    map service added

---
+++ src/app/canchas/services/map/map-service.ts
@@ -0,0 +1,13 @@
+import { inject, Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AddressRequest } from '../../models/address-request';
+
+@Service()
+export class MapService {
+  private readonly url = "http://localhost:8080/map";
+  private readonly http = inject(HttpClient);
+
+  getAutocomplete(address: string) {
+    return this.http.get<AddressRequest[]>(`${this.url}/autocomplete/${address}`);
+  }
+}
