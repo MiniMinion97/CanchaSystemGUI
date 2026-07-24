4a674dc8ac154638cb7778b69ba1d482570f4a2d
6-LL <lorenzanilautaro+github@gmail.com>
2026-07-24 18:33:33 -0300

    address request & response added

---
+++ src/app/canchas/models/address-request.ts
@@ -0,0 +1,5 @@
+export interface AddressRequest {
+  address: string;
+  lat: number;
+  lng: number;
+}
---
+++ src/app/canchas/models/address-response.ts
@@ -0,0 +1,6 @@
+export interface AddressResponse {
+  id: number;
+  address: string;
+  lat: number;
+  lng: number;
+}
