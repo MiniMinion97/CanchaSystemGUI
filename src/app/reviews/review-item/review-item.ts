d840d665cad3c74475316dd1572ac7f2ed7375bf
6-LL <lorenzanilautaro+github@gmail.com>
2026-08-07 12:16:06 -0300

    reviews now render profile picture, header z-index fixed

--- src/app/reviews/review-item/review-item.ts
+++ src/app/reviews/review-item/review-item.ts
@@ -1,6 +1,9 @@
-import { Component, input, output, ChangeDetectionStrategy } from '@angular/core';
+import {Component, input, output, ChangeDetectionStrategy, inject, signal} from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { ReviewResponse } from '../models/review-response';
+import {ImageService} from '../../image/services/image-service';
+import {Image} from '../../image/models/image';
+import {ClientService} from '../../AdminComponents/client-service';
 
 @Component({
   selector: 'app-review-item',
@@ -11,6 +14,10 @@ import { ReviewResponse } from '../models/review-response';
   styleUrls: ['./review-item.css'],
 })
 export class ReviewItem {
+  private readonly imageService = inject(ImageService);
+  private readonly clientService = inject(ClientService);
+  protected profilePicture = signal<Image | null>(null);
+
   readonly review = input.required<ReviewResponse>();
   readonly clientId = input<string | null>(null);
   readonly isLoggedIn = input<boolean>(false);
@@ -22,10 +29,36 @@ export class ReviewItem {
 
   /** Devuelve true si el review pertenece al usuario logueado */
   isOwner(): boolean {
-  const review = this.review();
-  const reviewClientId = review.clientId;  // directo, no review.client.id
-  const currentClientId = this.clientId();
+    const review = this.review();
+    const reviewClientId = review.clientId;  // directo, no review.client.id
+    const currentClientId = this.clientId();
 
-  return reviewClientId === currentClientId && !!currentClientId;
-}
+    return reviewClientId === currentClientId && !!currentClientId;
+  }
+
+  ngOnInit() {
+    this.loadProfilePicture(this.review().clientId);
+  }
+
+  private loadProfilePicture(id: string) {
+    this.clientService.getClient(id).subscribe({
+      next: data => {
+        this.imageService.getImagesByClient(data.username).subscribe({
+          next: (data) => {
+            this.profilePicture.set(data);
+          },
+          error: (err) => {
+            console.error('❌ Error loading images:', err);
+          }
+        })
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+  }
+
+  public getImage(imageId: number | string) {
+    return this.imageService.getImageUrl(imageId);
+  }
 }
